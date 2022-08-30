25c2c1544a0b5cd1cc310cd2c10e49eaed57aa96
uchuukaeru <hirata.motoki@gmail.com>
2022-08-30 19:36:42 +0900

    change files
    音楽再生の手法の変更

--- music.js
+++ music.js
@@ -1,18 +1,14 @@
-window.onload=function(){
-    window.AudioContext=window.AudioContext||window.webkitAudioContext;
-    const audioElement=document.querySelector("audio");
-    
+window.onload=function(){    
+    let bgm=new Audio("前奏曲-第7番.mp3");
+    bgm.loop=true;
     document.getElementById("start").onclick=function(){
-        const ctx=new AudioContext();    
-        const track=ctx.createMediaElementSource(audioElement);
         console.log("click start");
-        if(ctx.state==="suspended") ctx.resume();
-        track.connect(ctx.destination);
-        audioElement.play();
+        bgm.play();
     }
     document.getElementById("stop").onclick=function(){
         console.log("click stop");
-        audioElement.pause();
+        bgm.pause();
+        // audioElement.pause();
     }
 }
 
