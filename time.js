e9eddcc7f9a4fa2eac05aa6e05a5eddd58887406
uchuukaeru <hirata.motoki@gmail.com>
2022-08-30 17:21:19 +0900

    add files
    タイマー用のjsファイル
    htmlファイル

---
+++ time.js
@@ -0,0 +1,29 @@
+var timeALL=1500;
+
+ var bool_stop=false;
+
+function now(){
+ if(!bool_stop){
+  timeALL--;
+  var timeS=timeALL%60;
+  var timeM=Math.floor(timeALL/60);
+
+  var str_timeS=String(str_timeS);
+  var str_timeM=String(str_timeM);
+  if(timeS<10)str_timeS="0"+str_timeS;
+  if(timeM<10)str_timeS="0"+str_timeM;
+ 
+  document.getElementById("time").innerHTML=timeM+":"+timeS;
+  if(bool_stop==false)window.setTimeout(now, 100);
+ }
+}
+
+function start(){
+ bool_stop=false;
+ now();
+}
+
+function stop(){
+ bool_stop=true;
+ now();
+}
